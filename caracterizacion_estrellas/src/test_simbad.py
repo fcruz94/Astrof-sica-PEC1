ee47786702194403f46d31e9a06eb661e9e00bcf
fcruz94 <56622830+fcruz94@users.noreply.github.com>
2025-04-07 18:46:54 +0200

    Update test_simbad.py
    Revisado

--- caracterizacion_estrellas/src/test_simbad.py
+++ caracterizacion_estrellas/src/test_simbad.py
@@ -2,11 +2,12 @@
 
 from astroquery.simbad import Simbad
 
-# Crear una instancia personalizada
+# Crear una instancia 
 custom_simbad = Simbad()
+# Lista con los campos que se pueden obtener
 lista = custom_simbad.list_votable_fields()
 print(lista)
-
+# Estos son los campos de interés: velocidad radial, paralaje y magnitudes en B y V
 custom_simbad.add_votable_fields("rvz_radvel", "plx_value", "B", "V")
 
 # Consultar una estrella
@@ -14,10 +15,10 @@ result = custom_simbad.query_object("HD 103095")
 
 # Mostrar resultados
 #print(result)
-#print(result['MAIN_ID'][0])         # Nombre del objeto
-print(result['plx_value'][0])       # Paralaje en mas
-print(result['rvz_radvel'][0])      # Velocidad radial en km/s
-print(result['B'][0])          # Magnitud B
-print(result['V'][0])          # Magnitud V
+#print(result['MAIN_ID'][0])     # Nombre del objeto
+print(result['plx_value'][0])    # Paralaje en mas
+print(result['rvz_radvel'][0])   # Velocidad radial en km/s
+print(result['B'][0])            # Magnitud B
+print(result['V'][0])            # Magnitud V
 
 print(Simbad.list_columns("flux"))
