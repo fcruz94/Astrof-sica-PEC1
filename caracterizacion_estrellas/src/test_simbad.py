ac01433ac66725a6cff85fe89d9cc0cb703702e3
fcruz94 <francisco.jct@gmail.com>
2025-04-07 17:20:18 +0200

    modificados:     .spyproject/config/workspace.ini 	modificados:     caracterizacion_estrellas/src/test_simbad.py

--- caracterizacion_estrellas/src/test_simbad.py
+++ caracterizacion_estrellas/src/test_simbad.py
@@ -4,6 +4,9 @@ from astroquery.simbad import Simbad
 
 # Crear una instancia personalizada
 custom_simbad = Simbad()
+lista = custom_simbad.list_votable_fields()
+print(lista)
+
 custom_simbad.add_votable_fields("rvz_radvel", "plx_value", "B", "V")
 
 # Consultar una estrella
@@ -16,3 +19,5 @@ print(result['plx_value'][0])       # Paralaje en mas
 print(result['rvz_radvel'][0])      # Velocidad radial en km/s
 print(result['B'][0])          # Magnitud B
 print(result['V'][0])          # Magnitud V
+
+print(Simbad.list_columns("flux"))
